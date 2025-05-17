ee445925cf53f73d211ef020f7985431148e3884
Himanshu-Developer <desilentorder@gmail.com>
2025-05-17 13:57:39 +0530

    blog api add

--- desilentorder/urls.py
+++ desilentorder/urls.py
@@ -1,8 +1,10 @@
+# desilentbackend/urls.py (project-level)
+
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('users.urls')),
-    path('blogs/', include('blogs.urls')),
+    path('admin/', admin.site.urls),         # Django admin site
+    path('api/', include('users.urls')),     # API routes for users app: /api/...
+    path('blogger/', include('blogs.urls'))  # API routes for blogs app: /api/blogs/...
 ]
