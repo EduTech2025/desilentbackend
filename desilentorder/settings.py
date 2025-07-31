01630a39fc0239d348fbe00de8c29d71b0d413c5
EduTech2025 <desilentorder@gmail.com>
2025-07-31 08:41:36 +0530

    Update settings.py

--- desilentorder/settings.py
+++ desilentorder/settings.py
@@ -104,12 +104,24 @@ WSGI_APPLICATION = 'desilentorder.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'desilentdb',
+        'USER': 'desilentuser',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 # Maine daale hai
 AUTH_USER_MODEL = 'users.CustomUser'
