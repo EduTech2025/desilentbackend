b261c1ed6243cdaf4b7ab68b773088174461db3c
Himanshu-Developer <himanshuswami2810@gmail.com>
2025-08-10 20:06:13 +0530

    .

--- desilentorder/urls.py
+++ desilentorder/urls.py
@@ -5,11 +5,11 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),         # Django admin site
-    path('api/', include('users.urls')),     # API routes for users app: /api/...
+    path('api/users/', include('users.urls')),     # API routes for users app: /api/...
     path('blogger/', include('blogs.urls')),  # API routes for blogs app: /api/blogs/...       # all user APIs start with /api/users/
     path('api/contact/', include('contact.urls')),   # all contact APIs start with /api/contact/
     path('api/courses/', include('courses.urls')), 
-    path('pdf/', include('modules.urls')),
-    path('dashboard/', include('dashboard.urls')),
+    path('api/pdf/', include('modules.urls')),
+    path('api/dashboard/', include('dashboard.urls')),
     path('', include('myapp.urls')),
 ]
